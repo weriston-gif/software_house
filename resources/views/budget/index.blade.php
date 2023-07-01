5126d6c0d34442ab315bc093ef9e9df271e3af70
Weriston Alves <weristonalves1@gmail.com>
2023-07-01 09:59:14 -0300

    ajustes

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -1,23 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-    @section('title', 'orçamento')
-
-
-
-
+<x-guest-layout>
     <div id="toggleMyModal" class="modal" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -142,72 +123,4 @@
             </div>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script>
-        $(document).ready(function() {
-            $('#toggleMyModal').modal('show');
-        });
-
-
-        function limpa_formulario_cep() {
-            // Limpa valores dos campos de endereço.
-            $("#rua").val("");
-            $("#bairro").val("");
-            $("#cidade").val("");
-            $("#uf").val("");
-            $("#pais").val("");
-        }
-
-        // Quando o campo CEP perde o foco.
-        $("#cep").blur(function() {
-            // Nova variável "cep" somente com dígitos.
-            let cep = $(this).val().replace(/\D/g, '');
-
-            // Verifica se o campo CEP possui valor informado.
-            if (cep !== "") {
-                // Expressão regular para validar o CEP.
-                let validacep = /^[0-9]{8}$/;
-
-                // Valida o formato do CEP.
-                if (validacep.test(cep)) {
-                    // Preenche os campos com "..." enquanto consulta o serviço do ViaCEP.
-                    $("#rua").val("").addClass("animate-pulse bg-gray-200");
-                    $("#bairro").val("").addClass("animate-pulse bg-gray-200");
-                    $("#cidade").val("").addClass("animate-pulse bg-gray-200");
-                    $("#uf").val("").addClass("animate-pulse bg-gray-200");
-                    $("#pais").val("").addClass("animate-pulse bg-gray-200");
-
-
-                    // Consulta o webservice do ViaCEP.
-                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
-                        if (!("erro" in dados)) {
-                            // Atualiza os campos com os valores da consulta.
-                            $("#rua").val(dados.logradouro).removeClass("animate-pulse bg-gray-200");
-                            $("#bairro").val(dados.bairro).removeClass("animate-pulse bg-gray-200");
-                            $("#cidade").val(dados.localidade).removeClass("animate-pulse bg-gray-200");
-                            $("#uf").val(dados.uf).removeClass("animate-pulse bg-gray-200");
-                            $("#pais").val("Brasil").removeClass("animate-pulse bg-gray-200");
-
-                        } else {
-                            // CEP pesquisado não foi encontrado.
-                            limpa_formulario_cep();
-                            alert("CEP não encontrado.");
-                        }
-                    });
-                } else {
-                    // CEP é inválido.
-                    limpa_formulario_cep();
-                    alert("Formato de CEP inválido.");
-                }
-            } else {
-                // CEP sem valor, limpa o formulário.
-                limpa_formulario_cep();
-            }
-        });
-    </script>
-
-</body>
-
-</html>
+</x-guest-layout>
