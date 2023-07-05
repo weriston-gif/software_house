8be2615290a7bd8e0eb6abb4cf0271b1e5e617e6
Weriston Alves <weristonalves1@gmail.com>
2023-07-05 19:41:32 -0300

    ajustes

--- tests/App/Services/BudgetServiceTest.php
+++ tests/App/Services/BudgetServiceTest.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Models\Type;
+use App\Models\UserProjectBudget;
 use App\Models\UserProjectBudgetType;
 use App\Services\BudgetService;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
@@ -61,5 +63,65 @@ class BudgetServiceTest extends TestCase
         // Verifica se o valor total calculado está correto
         $expectedTotalValue = ($type->value_per_page * $valuePerPage) + $type->value_page_login;
         $this->assertEquals($expectedTotalValue, $totalValue);
+    }
+
+    public function testUpdateBudgetForUserType()
+    {
+        // Cria um registro fictício de UserProjectBudget para usar no teste
+        $budgetUser = UserProjectBudget::factory()->create();
+
+        // Cria um registro fictício de UserProjectBudgetType para usar no teste
+        $budgetUserType = UserProjectBudgetType::factory()->create();
+
+        // Dados fictícios para atualizar o UserProjectBudget
+        $dataUserPersona = [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'rua' => 'Street 1',
+            'bairro' => 'Neighborhood',
+            'numero' => '123',
+            'cep' => '12345-678',
+            'municipio' => 'City',
+            'uf' => 'State',
+            'pais' => 'Country',
+            'user_project_budget_type_id' => 1,
+        ];
+        // Dados fictícios para atualizar o UserProjectBudgetType
+        $dataUserTypes = [
+            'final_budget_value' => 100.0, // Insira aqui o valor correto para $totalValue
+            'value_total_page' => 10, // Insira aqui o valor correto para $valuePerPage
+            'type_id' => 1, // Insira aqui o valor correto para $type
+            'platform' => 'Some platform', // Insira aqui o valor correto para $platform
+            'page_login' => 1, // Insira aqui o valor correto para $valuePageLogin
+            'system_pay' => true, // Insira aqui o valor correto para $systemPay
+            'license_access' => true, // Insira aqui o valor correto para $licenseAccess
+            'printer' => true, // Insira aqui o valor correto para $printer
+            'operational_system' => 'Some OS', // Insira aqui o valor correto para $operationalSystem
+            'browser_support' => 'Some browser support', // Insira aqui o valor correto para $browserSupport
+        ];
+
+
+        // Mock da classe Notification
+        Notification::fake();
+
+        // Chama o método updateBudgetForUserType com os dados de teste
+        $service = new BudgetService();
+        $result = $service->updateBudgetForUserType($budgetUser->id, $budgetUserType->id, $dataUserPersona, $dataUserTypes);
+
+        // Verifica se o método retornou true (ou seja, a atualização foi bem-sucedida)
+        $this->assertTrue($result);
+
+        // Verifica se o UserProjectBudget foi atualizado corretamente
+        $updatedBudgetUser = UserProjectBudget::findOrFail($budgetUser->id);
+        $this->assertEquals($dataUserPersona['name'], $updatedBudgetUser->name);
+        $this->assertEquals($dataUserPersona['email'], $updatedBudgetUser->email);
+        // Verifique outros campos atualizados conforme necessário
+
+        // Verifica se o UserProjectBudgetType foi atualizado corretamente
+        $updatedBudgetUserType = UserProjectBudgetType::findOrFail($budgetUserType->id);
+        $this->assertEquals($dataUserTypes['type_id'], $updatedBudgetUserType->type_id);
+        // Verifique outros campos atualizados conforme necessário
+
+     
     }
 }
